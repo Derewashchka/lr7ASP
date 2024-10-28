c7d3fc5930ab6d4725f4a0e4dc33841b40515cc2
My comp <My comp@DESKTOP-HBDEDEI>
2024-10-28 22:44:48 +0200

    Add project files.

---
+++ Controllers/FileController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace lr7.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet("File/DownloadFile")]
+        public IActionResult DownloadFileForm()
+        {
+            return View("DownloadFileForm");
+        }
+
+        [HttpPost("File/DownloadFile")]
+        public IActionResult GenerateFile(string firstName, string lastName, string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                fileName = "DefaultFileName";
+            }
+
+            var fileContent = $"{firstName} {lastName}";
+            var byteArray = Encoding.UTF8.GetBytes(fileContent);
+            var file = new FileContentResult(byteArray, "text/plain")
+            {
+                FileDownloadName = $"{fileName}.txt"
+            };
+            return file;
+        }
+    }
+}
